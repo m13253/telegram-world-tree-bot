667ce377171baff04cdb60bb2ebcec96d7b9389f
Star Brilliant <m13253@hotmail.com>
2017-06-04 16:04:10 +0800

    Convert spaces to tabs

--- send_queue.go
+++ send_queue.go
@@ -19,152 +19,152 @@
 package main
 
 import (
-    "container/list"
-    "log"
-    "reflect"
-    "sync"
-    "sync/atomic"
-    "time"
-    // "gopkg.in/telegram-bot-api.v4"
+	"container/list"
+	"log"
+	"reflect"
+	"sync"
+	"sync/atomic"
+	"time"
+	// "gopkg.in/telegram-bot-api.v4"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 type sendQueueItem struct {
-    priority    int
-    msg_config  []tgbotapi.Chattable
-    msg_result  []*tgbotapi.Message
-    msg_errors  []error
-    msg_index   int
-    msg_finish  uintptr
-    callback    func ([]*tgbotapi.Message, []error)
+	priority    int
+	msg_config  []tgbotapi.Chattable
+	msg_result  []*tgbotapi.Message
+	msg_errors  []error
+	msg_index   int
+	msg_finish  uintptr
+	callback    func ([]*tgbotapi.Message, []error)
 }
 
 const (
-    QUEUE_PRIORITY_LOW    = 0
-    QUEUE_PRIORITY_NORMAL = 1
-    QUEUE_PRIORITY_HIGH   = 2
+	QUEUE_PRIORITY_LOW    = 0
+	QUEUE_PRIORITY_NORMAL = 1
+	QUEUE_PRIORITY_HIGH   = 2
 )
 
 type sendQueue struct {
-    bot         *tgbotapi.BotAPI
-    lock        *sync.Mutex
-    cv          *sync.Cond
-    low         *list.List
-    normal      *list.List
-    high        *list.List
+	bot         *tgbotapi.BotAPI
+	lock        *sync.Mutex
+	cv          *sync.Cond
+	low         *list.List
+	normal      *list.List
+	high        *list.List
 }
 
 func NewSendQueue(bot *tgbotapi.BotAPI) *sendQueue {
-    q := &sendQueue {
-        bot:    bot,
-        lock:   new(sync.Mutex),
-        cv:     sync.NewCond(new(sync.Mutex)),
-        low:    list.New(),
-        normal: list.New(),
-        high:   list.New(),
-    }
-    go q.dispatchMessages()
-    return q
+	q := &sendQueue {
+		bot:    bot,
+		lock:   new(sync.Mutex),
+		cv:     sync.NewCond(new(sync.Mutex)),
+		low:    list.New(),
+		normal: list.New(),
+		high:   list.New(),
+	}
+	go q.dispatchMessages()
+	return q
 }
 
 func (q *sendQueue) Send(priority int, msg_config []tgbotapi.Chattable, callback func ([]*tgbotapi.Message, []error)) {
-    item := &sendQueueItem {
-        priority:   priority,
-        msg_config: msg_config,
-        msg_result: make([]*tgbotapi.Message, len(msg_config)),
-        msg_errors: make([]error, len(msg_config)),
-        msg_index:  0,
-        msg_finish: 0,
-        callback:   callback,
-    }
-    var msg_list *list.List
-    switch priority {
-    case QUEUE_PRIORITY_LOW:
-        msg_list = q.low
-    case QUEUE_PRIORITY_NORMAL:
-        msg_list = q.normal
-    case QUEUE_PRIORITY_HIGH:
-        msg_list = q.high
-    default:
-        panic("Unknown priority")
-    }
-    log.Printf("[QueueSend] Begin")
-    q.lock.Lock()
-    msg_list.PushBack(item)
-    q.lock.Unlock()
-    q.cv.Signal()
-    log.Printf("[QueueSend] End")
+	item := &sendQueueItem {
+		priority:   priority,
+		msg_config: msg_config,
+		msg_result: make([]*tgbotapi.Message, len(msg_config)),
+		msg_errors: make([]error, len(msg_config)),
+		msg_index:  0,
+		msg_finish: 0,
+		callback:   callback,
+	}
+	var msg_list *list.List
+	switch priority {
+	case QUEUE_PRIORITY_LOW:
+		msg_list = q.low
+	case QUEUE_PRIORITY_NORMAL:
+		msg_list = q.normal
+	case QUEUE_PRIORITY_HIGH:
+		msg_list = q.high
+	default:
+		panic("Unknown priority")
+	}
+	log.Printf("[QueueSend] Begin")
+	q.lock.Lock()
+	msg_list.PushBack(item)
+	q.lock.Unlock()
+	q.cv.Signal()
+	log.Printf("[QueueSend] End")
 }
 
 func (q *sendQueue) dispatchMessages() {
-    for {
-        log.Printf("[QueueRecv] Begin")
-        q.lock.Lock()
-        if el := q.high.Front(); el != nil {
-            item := el.Value.(*sendQueueItem)
-            if item.msg_index == len(item.msg_config) {
-                q.high.Remove(el)
-                q.lock.Unlock()
-            } else {
-                q.lock.Unlock()
-                q.dispatchMessage(item)
-            }
-            log.Printf("[QueueRecv] High")
-        } else if el := q.normal.Front(); el != nil {
-            item := el.Value.(*sendQueueItem)
-            if item.msg_index == len(item.msg_config) {
-                q.normal.Remove(el)
-                q.lock.Unlock()
-            } else {
-                q.lock.Unlock()
-                q.dispatchMessage(item)
-            }
-            log.Printf("[QueueRecv] Normal")
-        } else if el := q.low.Front(); el != nil {
-            item := el.Value.(*sendQueueItem)
-            if item.msg_index == len(item.msg_config) {
-                q.low.Remove(el)
-                q.lock.Unlock()
-            } else {
-                q.lock.Unlock()
-                q.dispatchMessage(item)
-            }
-            log.Printf("[QueueRecv] Low")
-        } else {
-            log.Printf("[QueueRecv] Wait")
-            q.cv.L.Lock()
-            q.lock.Unlock()
-            q.cv.Wait()
-            q.cv.L.Unlock()
-            log.Printf("[QueueRecv] Wake")
-        }
-    }
+	for {
+		log.Printf("[QueueRecv] Begin")
+		q.lock.Lock()
+		if el := q.high.Front(); el != nil {
+			item := el.Value.(*sendQueueItem)
+			if item.msg_index == len(item.msg_config) {
+				q.high.Remove(el)
+				q.lock.Unlock()
+			} else {
+				q.lock.Unlock()
+				q.dispatchMessage(item)
+			}
+			log.Printf("[QueueRecv] High")
+		} else if el := q.normal.Front(); el != nil {
+			item := el.Value.(*sendQueueItem)
+			if item.msg_index == len(item.msg_config) {
+				q.normal.Remove(el)
+				q.lock.Unlock()
+			} else {
+				q.lock.Unlock()
+				q.dispatchMessage(item)
+			}
+			log.Printf("[QueueRecv] Normal")
+		} else if el := q.low.Front(); el != nil {
+			item := el.Value.(*sendQueueItem)
+			if item.msg_index == len(item.msg_config) {
+				q.low.Remove(el)
+				q.lock.Unlock()
+			} else {
+				q.lock.Unlock()
+				q.dispatchMessage(item)
+			}
+			log.Printf("[QueueRecv] Low")
+		} else {
+			log.Printf("[QueueRecv] Wait")
+			q.cv.L.Lock()
+			q.lock.Unlock()
+			q.cv.Wait()
+			q.cv.L.Unlock()
+			log.Printf("[QueueRecv] Wake")
+		}
+	}
 }
 
 func (q *sendQueue) dispatchMessage(item *sendQueueItem) {
-    i := item.msg_index
-    item.msg_index = i + 1
+	i := item.msg_index
+	item.msg_index = i + 1
 
-    delay := time.After(40 * time.Millisecond)
+	delay := time.After(40 * time.Millisecond)
 
-    go func() {
-        result := new(tgbotapi.Message)
-        var err error
-        *result, err = q.bot.Send(item.msg_config[i])
-        item.msg_result[i], item.msg_errors[i] = result, err
+	go func() {
+		result := new(tgbotapi.Message)
+		var err error
+		*result, err = q.bot.Send(item.msg_config[i])
+		item.msg_result[i], item.msg_errors[i] = result, err
 
-        if err != nil {
-            reflect_msg := reflect.ValueOf(item.msg_config[i])
-            chat_id := reflect_msg.FieldByName("ChatID").Interface()
-            log.Printf("Send to #%+v failed: %+v\n", chat_id, err)
-        }
+		if err != nil {
+			reflect_msg := reflect.ValueOf(item.msg_config[i])
+			chat_id := reflect_msg.FieldByName("ChatID").Interface()
+			log.Printf("Send to #%+v failed: %+v\n", chat_id, err)
+		}
 
-        if int(atomic.AddUintptr(&item.msg_finish, 1)) == len(item.msg_config) {
-            if item.callback != nil {
-                item.callback(item.msg_result, item.msg_errors)
-            }
-        }
-    } ()
+		if int(atomic.AddUintptr(&item.msg_finish, 1)) == len(item.msg_config) {
+			if item.callback != nil {
+				item.callback(item.msg_result, item.msg_errors)
+			}
+		}
+	} ()
 
-    <-delay
+	<-delay
 }
